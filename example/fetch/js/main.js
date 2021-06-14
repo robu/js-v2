d348f71d36520572d83b9fcbdd1f89301c2e124e
Kenneth Lewenhagen <lewenhagen@live.com>
2021-06-14 08:50:03 +0200

    more data in folder

--- example/fetch/js/main.js
+++ example/fetch/js/main.js
@@ -1,18 +1,31 @@
 (function () {
   'use strict'
 
-  const myContent = document.getElementById('content')
+  // const myContent = document.getElementById('content')
+  const staffTable = document.getElementById('staffTable')
 
-  myContent.innerHTML = '<h3>This is a template!</h3>'
+  // myContent.innerHTML = '<h3>This is a template!</h3>'
 
   // fetch('https://api.carbonintensity.org.uk/intensity/factors/')
   //     .then(response => response.json())
   //     .then(json => console.log(json.data[0]))
 
-  fetch('data/staff.json', {mode: 'no-cors'})
-      .then(response => response.json())
-      .then(json => console.log(json))
+  fetch('https://raw.githubusercontent.com/dbwebb-se/js-v2/master/example/fetch/data/staff.json')
+    .then(response => response.json())
+    .then(function (json) {
+      staffTable.innerHTML = `<thead><tr><th>Firstname</th><th>Lastname</th></tr></thead>`
+      staffTable.innerHTML += `<tbody>`
+      for (const teacher of json) {
+        staffTable.innerHTML += `
+                <tr>
+                    <td>${teacher.firstname}</td>
+                    <td>${teacher.lastname}</td>
+                </tr>`
+      }
+      staffTable.innerHTML += `</tbody>`
 
-// https://carbonintensity.org.uk/
+    })
+
+  // https://carbonintensity.org.uk/
   window.console.log('Sandbox is ready!')
 })()
