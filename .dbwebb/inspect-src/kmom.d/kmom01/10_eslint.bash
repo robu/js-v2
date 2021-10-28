e941efe64b371a0636b94aaadd2c3185c71272f2
Kenneth Lewenhagen <lewenhagen@live.com>
2021-10-28 15:42:36 +0200

    added check on folder, files

--- .dbwebb/inspect-src/kmom.d/kmom01/10_eslint.bash
+++ .dbwebb/inspect-src/kmom.d/kmom01/10_eslint.bash
@@ -8,15 +8,28 @@ normal=$(tput sgr 0)
 function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
-#
-# header "LINTING CODE"
 
-cp example/environment/.eslintrc.json me/ || exit 1
-cp example/environment/package.json me/ || exit 1
-cd me || exit 1
+if [[ ! -f "package.json" ]] || [[ ! -f ".eslintrc.json" ]]; then
+    read -p "Missing files. Should I fix it? [Y/n]" answer
+    if [[ "$answer" != "n" ]]; then
+        cp example/environment/.eslintrc.json .
+        cp example/environment/package.json .
+        npm install
+    else
+        exit 1
+    fi
+fi
 
-npm install
-npm run linter "kmom01/" --silent || exit 1
+if [[ ! -d "node_modules" ]]; then
+    read -p "Missing folder npm_modules. Should I fix it? [Y/n]" answer
+    if [[ "$answer" != "n" ]]; then
+        npm install
+    else
+        exit 1
+    fi
+fi
+
+npm run linter "me/kmom01/" --silent || exit 1
 
 read -p "Done? "
 
--- .dbwebb/inspect-src/kmom.d/kmom02/10_eslint.bash
+++ .dbwebb/inspect-src/kmom.d/kmom02/10_eslint.bash
@@ -8,16 +8,31 @@ normal=$(tput sgr 0)
 function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
-#
-# header "LINTING CODE"
 
-# cp example/environment/.eslintrc.json . || exit 1
-# cp example/environment/package.json . || exit 1
-# cd me || exit 1
 
-# ls -al
 
-# npm install
+if [[ ! -f "package.json" ]] || [[ ! -f ".eslintrc.json" ]]; then
+    read -p "Missing files. Should I fix it? [Y/n]" answer
+    if [[ "$answer" != "n" ]]; then
+        cp example/environment/.eslintrc.json .
+        cp example/environment/package.json .
+        npm install
+    else
+        exit 1
+    fi
+fi
+
+if [[ ! -d "node_modules" ]]; then
+    read -p "Missing folder npm_modules. Should I fix it? [Y/n]" answer
+    if [[ "$answer" != "n" ]]; then
+        npm install
+    else
+        exit 1
+    fi
+fi
+
+
+
 npm run linter "me/kmom02/" --silent || exit 1
 
 read -p "Done? "
