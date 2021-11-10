e9d282765b2eafd1567f5f03029fe75129eb3a46
Kenneth Lewenhagen <lewenhagen@live.com>
2021-11-10 15:07:55 +0100

    3456632

--- .dbwebb/inspect-src/kmom.d/kmom03/pre.bash
+++ .dbwebb/inspect-src/kmom.d/kmom03/pre.bash
@@ -5,37 +5,11 @@ green=$(tput setaf 2)
 cyan=$(tput setaf 6)
 normal=$(tput sgr 0)
 
-function printthisfile
-{
-    printf "\n${cyan}"
-    more "$1"
-    printf "${normal}\n"
+function header {
+    printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
 
-printf ">>> -------------- Pre inspect -------------------------\n"
+cd me || exit 1
 
-cd me/kmom03/script
-
-success=0
-
-if [[ -f  "dockerhub.txt" ]]; then
-    url=""
-    echo ""
-    read -p "Press any key to view dockerhub.txt"
-    printthisfile "dockerhub.txt"
-
-    read -p "Good to go? [Y/n]" answer
-
-    if [[ "$answer" != "n" ]]; then
-        url=$(< dockerhub.txt)
-    else
-        read -p "Type the information manually (username/imagename:tag): " url
-    fi
-
-    docker run --rm -it "$url"
-    read -p "Press any key to delete the image."
-    docker rmi -f "$url"
-    read -p "Image gone. Press any key to move on."
-fi
-
-exit $success
+header "OPENING FILES"
+atom kmom03
