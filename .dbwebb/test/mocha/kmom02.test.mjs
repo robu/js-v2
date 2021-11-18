ba57a63b23bfae4d11ebb14f3f8f42679226e9f6
Kenneth Lewenhagen <lewenhagen@live.com>
2021-11-18 10:58:02 +0100

    181121

--- .dbwebb/test/mocha/kmom02.test.mjs
+++ .dbwebb/test/mocha/kmom02.test.mjs
@@ -4,7 +4,7 @@ import chaiFiles from 'chai-files';
 import chaiExecAsync from '@jsdevtools/chai-exec';
 import { SOURCE, SOLUTION, ERROR_MSG } from './config.mjs';
 
-import { animals as african } from '../../../example/kmom02/african.mjs'
+// import { animals as african } from '../../../example/kmom02/african.mjs'
 // import * as european from '../../../me/kmom02/european.mjs'
 // import * as american from '../../../me/kmom02/american.mjs'
 import * as report from '../../../me/kmom02/report.mjs'
@@ -33,54 +33,54 @@ describe('Testsuite for KMOM02', () => {
 
         before(function () {
             chai.config.includeStack = false;
-            americanAnimals = helpers.allAnimals("america")
-            africanAnimals = helpers.allAnimals("africa")
+            // americanAnimals = helpers.allAnimals("america")
+            // africanAnimals = helpers.allAnimals("africa")
             europeanAnimals = helpers.allAnimals("europe")
         });
 
         beforeEach(function() {
-            // americanAnimals = helpers.allAnimals("america")
-            // africanAnimals = helpers.allAnimals("africa")
-            // //helpers.allAnimals("africa")
-            // // console.log(africanAnimals);
-            // europeanAnimals = helpers.allAnimals("europe")
+            americanAnimals = helpers.allAnimals("america")
+            africanAnimals = helpers.allAnimals("africa")
+            europeanAnimals = helpers.allAnimals("europe")
+            //helpers.allAnimals("africa")
+            // console.log(africanAnimals);
         })
 
         describe("all", () => {
 
-            it('should return an array of american animals', (done) => {
-                const lowercased = americanAnimals.map(name => name.toLowerCase());
-
-                expect(lowercased).to.have.members([
-                    'Florida panther',
-                    'Lesser prairie chicken',
-                    "Devil's Hole pupfish",
-                    "Bryde's whale",
-                    'North Atlantic right whale',
-                    'Monarch butterfly',
-                    'Delta Smelt',
-                    'Giant sea bass',
-                    "Franklin's bumble bee"
-                ].map(name => name.toLowerCase()));
-                done();
-            });
-
-            it('should return an array of african animals', (done) => {
-                const lowercased = africanAnimals.map(name => name.toLowerCase());
-                expect(lowercased).to.have.members([
-                  'Ethiopian wolf',
-                  'Pangolin',
-                  'Black rhino',
-                  'White rhino',
-                  'Mountain gorilla',
-                  'African wild dog',
-                  'African penguin',
-                  'Giraffe',
-                  'Hooded vulture',
-                  'Chimpanzee'
-                ].map(name => name.toLowerCase()));
-                done();
-            });
+            // it('should return an array of american animals', (done) => {
+            //     const lowercased = americanAnimals.map(name => name.toLowerCase());
+            //
+            //     expect(lowercased).to.have.members([
+            //         'Florida panther',
+            //         'Lesser prairie chicken',
+            //         "Devil's Hole pupfish",
+            //         "Bryde's whale",
+            //         'North Atlantic right whale',
+            //         'Monarch butterfly',
+            //         'Delta Smelt',
+            //         'Giant sea bass',
+            //         "Franklin's bumble bee"
+            //     ].map(name => name.toLowerCase()));
+            //     done();
+            // });
+            //
+            // it('should return an array of african animals', (done) => {
+            //     const lowercased = africanAnimals.map(name => name.toLowerCase());
+            //     expect(lowercased).to.have.members([
+            //       'Ethiopian wolf',
+            //       'Pangolin',
+            //       'Black rhino',
+            //       'White rhino',
+            //       'Mountain gorilla',
+            //       'African wild dog',
+            //       'African penguin',
+            //       'Giraffe',
+            //       'Hooded vulture',
+            //       'Chimpanzee'
+            //     ].map(name => name.toLowerCase()));
+            //     done();
+            // });
 
             it('should return an array of european animals', (done) => {
                 const lowercased = europeanAnimals.map(name => name.toLowerCase());
@@ -103,21 +103,21 @@ describe('Testsuite for KMOM02', () => {
 
         describe("random", () => {
 
-            it('should return a random animal (america)', (done) => {
-                const randAnim = helpers.getRandomAnimal(americanAnimals)
+            // it('should return a random animal (america)', (done) => {
+            //     const randAnim = helpers.getRandomAnimal(americanAnimals)
+            //
+            //     expect(americanAnimals).to.include(randAnim);
+            //     done();
+            // });
+            //
+            // it('should return a random animal (africa)', (done) => {
+            //     const randAnim = helpers.getRandomAnimal(africanAnimals)
+            //
+            //     expect(africanAnimals).to.include(randAnim);
+            //     done();
+            // });
 
-                expect(americanAnimals).to.include(randAnim);
-                done();
-            });
-
-            it('should return a random animal (africa)', (done) => {
-                const randAnim = helpers.getRandomAnimal(africanAnimals)
-
-                expect(africanAnimals).to.include(randAnim);
-                done();
-            });
-
-            it('should return a random animal (europe)', (done) => {
+            it('should return a random animal', (done) => {
                 const randAnim = helpers.getRandomAnimal(europeanAnimals)
 
                 expect(europeanAnimals).to.include(randAnim);
@@ -127,21 +127,21 @@ describe('Testsuite for KMOM02', () => {
 
         describe("add", () => {
 
-            it('should contain the added super animal (america)', (done) => {
-                helpers.addAnimal(americanAnimals, 'capybara')
+            // it('should contain the added super animal (america)', (done) => {
+            //     helpers.addAnimal(americanAnimals, 'capybara')
+            //
+            //     expect(americanAnimals).to.include('capybara');
+            //     done();
+            // });
+            //
+            // it('should contain the added super animal (africa)', (done) => {
+            //     helpers.addAnimal(africanAnimals, 'capybara')
+            //
+            //     expect(africanAnimals).to.include('capybara');
+            //     done();
+            // });
 
-                expect(americanAnimals).to.include('capybara');
-                done();
-            });
-
-            it('should contain the added super animal (africa)', (done) => {
-                helpers.addAnimal(africanAnimals, 'capybara')
-
-                expect(africanAnimals).to.include('capybara');
-                done();
-            });
-
-            it('should contain the added super animal (europe)', (done) => {
+            it('should contain the added super animal', (done) => {
                 helpers.addAnimal(europeanAnimals, 'capybara')
 
                 expect(europeanAnimals).to.include('capybara');
@@ -150,23 +150,23 @@ describe('Testsuite for KMOM02', () => {
         });
 
         describe("remove", () => {
-            it('should not contain the removed animal (america)', (done) => {
-                let msg = helpers.removeAnimal(americanAnimals, 'Delta Smelt')
+            // it('should not contain the removed animal (america)', (done) => {
+            //     let msg = helpers.removeAnimal(americanAnimals, 'Delta Smelt')
+            //
+            //     expect(americanAnimals).to.not.include('Delta Smelt');
+            //     expect(msg).to.equal('Removed: Delta Smelt');
+            //     done();
+            // });
+            //
+            // it('should not contain the removed animal (africa)', (done) => {
+            //     let msg = helpers.removeAnimal(africanAnimals, 'Pangolin')
+            //
+            //     expect(africanAnimals).to.not.include('Pangolin');
+            //     expect(msg).to.equal('Removed: Pangolin');
+            //     done();
+            // });
 
-                expect(americanAnimals).to.not.include('Delta Smelt');
-                expect(msg).to.equal('Removed: Delta Smelt');
-                done();
-            });
-
-            it('should not contain the removed animal (africa)', (done) => {
-                let msg = helpers.removeAnimal(africanAnimals, 'Pangolin')
-
-                expect(africanAnimals).to.not.include('Pangolin');
-                expect(msg).to.equal('Removed: Pangolin');
-                done();
-            });
-
-            it('should not contain the removed animal (europe)', (done) => {
+            it('should not contain the removed animal', (done) => {
                 let msg = helpers.removeAnimal(europeanAnimals, 'Polar bear')
 
                 expect(europeanAnimals).to.not.include('Polar bear');
@@ -176,107 +176,111 @@ describe('Testsuite for KMOM02', () => {
         });
 
         describe("remove (non existent animal)", () => {
-            it('should give correct feedback (america)', (done) => {
-                let msg = helpers.removeAnimal(americanAnimals, 'Andreas')
+            // it('should give correct feedback (america)', (done) => {
+            //     let msg = helpers.removeAnimal(americanAnimals, 'Andreas')
+            //
+            //     expect(msg).to.equal('No "Andreas" in array.');
+            //     done();
+            // });
+            //
+            // it('should give correct feedback (africa)', (done) => {
+            //     let msg = helpers.removeAnimal(africanAnimals, 'Emil')
+            //
+            //     expect(msg).to.equal('No "Emil" in array.');
+            //     done();
+            // });
 
-                expect(msg).to.equal('No "Andreas" in array.');
-                done();
-            });
+            it('should give correct feedback', (done) => {
+                let msg = helpers.removeAnimal(europeanAnimals, 'Kangaroo')
 
-            it('should give correct feedback (africa)', (done) => {
-                let msg = helpers.removeAnimal(africanAnimals, 'Emil')
-
-                expect(msg).to.equal('No "Emil" in array.');
-                done();
-            });
-
-            it('should give correct feedback (europe)', (done) => {
-                let msg = helpers.removeAnimal(europeanAnimals, 'Mikael')
-
-                expect(msg).to.equal('No "Mikael" in array.');
+                expect(msg).to.equal('No "Kangaroo" in array.');
                 done();
             });
         });
 
         describe("prettyPrint", () => {
-            it('should match output (america)', (done) => {
-                // americanAnimals = helpers.allAnimals("america");
-                const msg = report.prettyPrint('American', americanAnimals)
+//             it('should match output (america)', (done) => {
+//                 // americanAnimals = helpers.allAnimals("america");
+//                 const msg = report.prettyPrint('American', americanAnimals)
+//
+//                 expect(msg).to.equal(`--- REPORT ---
+// American animals
+//
+// * Florida panther
+// * Lesser prairie chicken
+// * Devil's Hole pupfish
+// * Bryde's whale
+// * North Atlantic right whale
+// * Monarch butterfly
+// * Delta Smelt
+// * Giant sea bass
+// * Franklin's bumble bee
+// `);
+//                 done();
+//             });
+//
+//             it('should match output (africa)', (done) => {
+//                 const msg = report.prettyPrint('African', africanAnimals)
+//
+//                 expect(msg).to.equal(`--- REPORT ---
+// African animals
+//
+// * Ethiopian wolf
+// * Pangolin
+// * Black rhino
+// * White rhino
+// * Mountain gorilla
+// * African wild dog
+// * African penguin
+// * Giraffe
+// * Hooded vulture
+// * Chimpanzee
+// `);
+//                 done();
+//             });
+
+            it('should match output', (done) => {
+                const msg = report.prettyPrint('Test', africanAnimals.map(function(item) {
+                    return item.toLowerCase()
+                }))
 
                 expect(msg).to.equal(`--- REPORT ---
-American animals
+Test animals
 
-* Florida panther
-* Lesser prairie chicken
-* Devil's Hole pupfish
-* Bryde's whale
-* North Atlantic right whale
-* Monarch butterfly
-* Giant sea bass
-* Franklin's bumble bee
-* capybara
-`);
-                done();
-            });
-
-            it('should match output (africa)', (done) => {
-                const msg = report.prettyPrint('African', africanAnimals)
-
-                expect(msg).to.equal(`--- REPORT ---
-African animals
-
-* Ethiopian wolf
-* Black rhino
-* White rhino
-* Mountain gorilla
-* African wild dog
-* African penguin
-* Giraffe
-* Hooded vulture
-* Chimpanzee
-* capybara
-`);
-                done();
-            });
-
-            it('should match output (europe)', (done) => {
-                const msg = report.prettyPrint('European', europeanAnimals)
-
-                expect(msg).to.equal(`--- REPORT ---
-European animals
-
-* Hooded seal
-* Blue whale
-* Golden eagle
-* Lynx
-* Crayfish
-* Amazon ant
-* European mink
-* Gerfalcon
-* Long-fingered bat
-* capybara
-`);
+* ethiopian wolf
+* pangolin
+* black rhino
+* white rhino
+* mountain gorilla
+* african wild dog
+* african penguin
+* giraffe
+* hooded vulture
+* chimpanzee`);
                 done();
             });
         });
 
         describe("fixFirstLetter", () => {
 
-            it('should make first letter uppercase (america)', (done) => {
-                let fixed = report.fixFirstLetter(americanAnimals)
+            // it('should make first letter uppercase (america)', (done) => {
+            //     helpers.addAnimal(americanAnimals, "capybara");
+            //     let fixed = report.fixFirstLetter(americanAnimals);
+            //
+            //     expect(fixed).to.include('Capybara');
+            //     done();
+            // });
+            //
+            // it('should make first letter uppercase (africa)', (done) => {
+            //     helpers.addAnimal(africanAnimals, "capybara");
+            //     let fixed = report.fixFirstLetter(africanAnimals)
+            //
+            //     expect(fixed).to.include('Capybara');
+            //     done();
+            // });
 
-                expect(fixed).to.include('Capybara');
-                done();
-            });
-
-            it('should make first letter uppercase (africa)', (done) => {
-                let fixed = report.fixFirstLetter(africanAnimals)
-
-                expect(fixed).to.include('Capybara');
-                done();
-            });
-
-            it('should make first letter uppercase (europe)', (done) => {
+            it('should make first letter uppercase', (done) => {
+                helpers.addAnimal(europeanAnimals, "capybara");
                 let fixed = report.fixFirstLetter(europeanAnimals)
 
                 expect(fixed).to.include('Capybara');
@@ -288,11 +292,11 @@ European animals
 
         describe("sort", () => {
             it('should sort the array', (done) => {
-                let americanAnimals2 = helpers.allAnimals("america");
-                helpers.addAnimal(americanAnimals2, "capybara");
-                report.sortAnimals(americanAnimals2);
+                // let americanAnimals2 = helpers.allAnimals("america");
+                helpers.addAnimal(americanAnimals, "capybara");
+                report.sortAnimals(americanAnimals);
 
-                expect(americanAnimals2).to.have.ordered.members([
+                expect(americanAnimals).to.have.ordered.members([
                   "Bryde's whale",
                   "Delta Smelt",
                   "Devil's Hole pupfish",
@@ -310,9 +314,9 @@ European animals
 
         describe("filter", () => {
             it('should filter out animals with more than 10 letters', (done) => {
-                americanAnimals = helpers.allAnimals("america");
-                europeanAnimals = helpers.allAnimals("europe");
-                africanAnimals = helpers.allAnimals("africa");
+                // americanAnimals = helpers.allAnimals("america");
+                // europeanAnimals = helpers.allAnimals("europe");
+                // africanAnimals = helpers.allAnimals("africa");
                 let filtered = report.filterAnimals([americanAnimals, africanAnimals, europeanAnimals], 10)
 
                 expect(filtered).to.have.members([
