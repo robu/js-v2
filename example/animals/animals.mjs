22a9680d77826be502299db38dc2d7ed48e41cd9
Kenneth Lewenhagen <lewenhagen@live.com>
2022-10-19 11:57:09 +0200

    some examples

---
+++ example/animals/animals.mjs
@@ -0,0 +1,47 @@
+/**
+ * @module animals
+ * @ignore
+ */
+
+/** @type {array} */
+const africanAnimals = [
+    'Ethiopian wolf',
+    'Pangolin',
+    'Black rhino',
+    'White rhino',
+    'Mountain gorilla',
+    'African wild dog',
+    'African penguin',
+    'Giraffe',
+    'Hooded vulture',
+    'Chimpanzee'
+];
+
+/** @type {array} */
+const americanAnimals = [
+    'Florida panther',
+    'Lesser prairie chicken',
+    "Devil's Hole pupfish",
+    "Bryde's whale",
+    'North Atlantic right whale',
+    'Monarch butterfly',
+    'Delta Smelt',
+    'Giant sea bass',
+    "Franklin's bumble bee"
+];
+
+/** @type {array} */
+const europeanAnimals = [
+    'Hooded seal',
+    'Blue whale',
+    'Polar bear',
+    'Golden eagle',
+    'Lynx',
+    'Crayfish',
+    'Amazon ant',
+    'European mink',
+    'Gerfalcon',
+    'Long-fingered bat'
+];
+
+export { africanAnimals, americanAnimals, europeanAnimals };
