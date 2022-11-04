8ac4433b5df2068c68902bb98d10f3d29f05e215
lewenhagen <lewenhagen@live.com>
2022-11-04 07:57:14 +0100

    added to solutions

--- .dbwebb/inspect-src/kmom.d/kmom01/pre.bash
+++ .dbwebb/inspect-src/kmom.d/kmom01/pre.bash
@@ -9,6 +9,13 @@ function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
 
+printf ">>> -------------- dbwebb test kmom01 -------------------------\n"
+
+dbwebb test "kmom01"
+
+echo "Press any key to continue."
+read
+
 cd me || exit 1
 
 header "OPENING FILES"
