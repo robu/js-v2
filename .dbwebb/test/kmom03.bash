05f15ee4af68e3b99a7ae194fa6300ca5831fa7e
lewenhagen <lewenhagen@live.com>
2022-11-09 14:29:36 +0100

    added test kmom03

---
+++ .dbwebb/test/kmom03.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+. "functions.bash"
+
+declare -a files=(
+    "js/main.js"
+    "style/style.css"
+    "index.html"
+    "jsdoc/index.html"
+    "../redovisa/index.html"
+    )
+
+# Print the header for the testsuite
+header "$1" "$2" "$3"
+
+# CHeck if the files exists and have correct filename
+checkIfFilesExist "${files[@]}"
+
+exit "$(isSuccess)"
