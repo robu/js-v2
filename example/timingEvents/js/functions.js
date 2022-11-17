08e285f6ba91e8fe92e044d64aae12aa3df677b1
lewenhagen <lewenhagen@live.com>
2022-11-17 15:41:06 +0100

    removed useless file

--- example/timingEvents/js/functions.js
+++
@@ -1,9 +0,0 @@
-window.localStorage.setItem('name', 'Murphy');
-window.localStorage.profession = `Stormtrooper`;
-
-// Get items
-const name = window.localStorage.getItem('name');
-const profession = window.localStorage.profession;
-
-console.log(name);
-console.log(profession);
