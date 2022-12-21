4f8527b80de31394ddb61eefc4cb454dde11cf5a
lewenhagen <lewenhagen@live.com>
2022-12-21 15:14:40 +0100

    544324

---
+++ .dbwebb/inspect-src/kmom.d/kmom04/20_test.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+printf ">>> -------------- dbwebb test kmom04 -------------------------\n"
+
+dbwebb test "kmom04"
+
+echo "Press any key to continue."
+read
--- .dbwebb/inspect-src/kmom.d/kmom06/10_eslint.bash
+++ .dbwebb/inspect-src/kmom.d/kmom06/10_eslint.bash
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-cyan=$(tput setaf 6)
-normal=$(tput sgr 0)
-
 function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
