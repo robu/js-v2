9f78474f3960e2371b208d2095e0e607e6d0cc9b
Kenneth Lewenhagen <lewenhagen@live.com>
2023-06-29 14:50:09 +0200

    asdf

---
+++ docs/example/animals/animals.js
@@ -0,0 +1,44 @@
+/**
+ * @module animals
+ * @ignore
+ */
+
+const africanAnimals = [
+  'Ethiopian wolf',
+  'Pangolin',
+  'Black rhino',
+  'White rhino',
+  'Mountain gorilla',
+  'African wild dog',
+  'African penguin',
+  'Giraffe',
+  'Hooded vulture',
+  'Chimpanzee'
+]
+
+const americanAnimals = [
+  'Florida panther',
+  'Lesser prairie chicken',
+  "Devil's Hole pupfish",
+  "Bryde's whale",
+  'North Atlantic right whale',
+  'Monarch butterfly',
+  'Delta Smelt',
+  'Giant sea bass',
+  "Franklin's bumble bee"
+]
+
+const europeanAnimals = [
+  'Hooded seal',
+  'Blue whale',
+  'Polar bear',
+  'Golden eagle',
+  'Lynx',
+  'Crayfish',
+  'Amazon ant',
+  'European mink',
+  'Gerfalcon',
+  'Long-fingered bat'
+]
+
+export { africanAnimals, americanAnimals, europeanAnimals }
