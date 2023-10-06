15e6ecbc3559f4971fe41dedb139e4686b91a10d
Kenneth Lewenhagen <lewenhagen@live.com>
2023-10-06 11:27:06 +0200

    added args example

---
+++ example/node/args.js
@@ -0,0 +1,13 @@
+/** All arguments
+ * 0 /usr/bin/node
+ * 1 absolute path
+ * 2 first argument
+ * n n:th argument
+*/ 
+
+let arg = process.argv
+
+console.log(arg)
+console.log(arg[0])
+console.log(arg[1])
+console.log(arg[2])
